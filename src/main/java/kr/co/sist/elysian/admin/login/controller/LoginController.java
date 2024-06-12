8f9c3e759e60fa4a33126009ac4ac128041a1888
iuiwiff <jimin5305@naver.com>
2024-06-12 16:44:25 +0900

    temp

--- src/main/java/kr/co/sist/elysian/admin/login/controller/LoginController.java
+++ src/main/java/kr/co/sist/elysian/admin/login/controller/LoginController.java
@@ -1,20 +1,40 @@
 package kr.co.sist.elysian.admin.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+@SessionAttributes("admin_id")
 @Controller("adminLoginController")
 @RequestMapping("/admin")
 public class LoginController {
 
+	@GetMapping("/adminlogin_frm.do") 
+	public String adminMain() {
+		return "admin/index";
+	}
 	
 	@GetMapping("/dashboard.do")
-	public String login() {
+	public String dashboard() {
 
 		return "admin/dashboard/dashboard";
 
-	}// main
+	}// login
+	
+	@GetMapping("/set_session.do")
+	public String loginProcess(Model model) {
+		model.addAttribute("admin_id", "admin");
+		return "forward:dashboard.do";
+	}
+	
+	@GetMapping("/remove_session.do")
+	public String logout(SessionStatus ss) {
+		ss.setComplete();
+		return "redirect:adminlogin_frm.do";
+	}
 	
 	
 	

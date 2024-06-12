92460760221631f312c7f684d2d6206bdab7cb7e
iuiwiff <jimin5305@naver.com>
2024-06-12 17:30:41 +0900

    push
    수정

--- src/main/java/kr/co/sist/elysian/admin/login/controller/LoginController.java
+++ src/main/java/kr/co/sist/elysian/admin/login/controller/LoginController.java
@@ -1,13 +1,19 @@
 package kr.co.sist.elysian.admin.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+@SessionAttributes("admin_id")
 @Controller("adminLoginController")
 @RequestMapping("/admin")
 public class LoginController {
 
+	
+	
 	@GetMapping("/login_frm.do")
 	public String main() {
 
@@ -15,13 +21,26 @@ public class LoginController {
 
 	}// main
 	
+	
 
 	@GetMapping("/dashboard.do")
-	public String login() {
+	public String dashboard() {
 
 		return "admin/dashboard/dashboard";
 
 	}// login
+	
+	@GetMapping("/set_session.do")
+	public String loginProcess(Model model) {
+		model.addAttribute("admin_id", "admin");
+		return "forward:dashboard.do";
+	}
+	
+	@GetMapping("/remove_session.do")
+	public String logout(SessionStatus ss) {
+		ss.setComplete();
+		return "redirect:adminlogin_frm.do";
+	}
 	
 	
 	

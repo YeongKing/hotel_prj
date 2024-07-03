6bd4c3df773cca76736fceacf69b0edd5e822e05
dbsdndcks <ywcsuwon@naver.com>
2024-07-04 03:46:35 +0900

    이벤트 목록 ( 더보기  비동기) 이벤트 소제목 컬럼 및 관리자에서 소제목부분 추가
    이벤트 목록 ( 더보기  비동기) 이벤트 소제목 컬럼 및 관리자에서 소제목부분 추가

--- src/main/java/kr/co/sist/elysian/admin/board/event/model/vo/EventVO.java
+++ src/main/java/kr/co/sist/elysian/admin/board/event/model/vo/EventVO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class EventVO {
 
-	private String eventNum, eventTitle,adminId, eventStartDate, eventEndDate , eventMainImg, eventSubImg,eventContent;
+	private String eventNum, eventTitle,eventSubTitle, adminId, eventStartDate, eventEndDate , eventMainImg, eventSubImg,eventContent;
 	
 	
 	

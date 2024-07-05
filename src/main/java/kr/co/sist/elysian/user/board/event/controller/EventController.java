67c7d7e622483debb990f54d7c13c8c9dc5a7797
dbsdndcks <ywcsuwon@naver.com>
2024-07-05 16:44:32 +0900

    이벤트 검색기능 추가 , 관리자 다이닝 수정기능 수정
    이벤트 검색기능 추가 , 관리자 다이닝 수정기능 수정

--- src/main/java/kr/co/sist/elysian/user/board/event/controller/EventController.java
+++ src/main/java/kr/co/sist/elysian/user/board/event/controller/EventController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class EventController {
 	@Autowired(required = false)
 	private EventService eventService;
 	
+	
 	/**
 	 * 이벤트리스트 화면으로 가는 메서드
 	 * @return
@@ -33,6 +35,13 @@ public class EventController {
 	} // searchAllEventList
 	
 	
+	@GetMapping("/eventDetail.do")
+	public String goEventDetailPage(@RequestParam String eventNum,Model model) {
+		
+		return "user/board/eventDetail";
+	}
+	
+	
 	/**
 	 * 이벤트리스트를 가져오는 메서드
 	 * @param page
@@ -43,16 +52,30 @@ public class EventController {
 	@GetMapping("/eventList.do")
 	public Map<String, Object> searchEventLists(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "6") int size){
+            @RequestParam(defaultValue = "6") int size,
+            @RequestParam String startDate,
+            @RequestParam String endDate,
+            @RequestParam String searchText){
 		
-		List<EventDomain> events =	eventService.searchAllEventLists(page,size);
-		int totalCount = eventService.getTotalCount();
+		List<EventDomain> events = null;
+		int totalCount = 0;
+		if(startDate == "" || endDate == "") {
+			events =	eventService.searchAllEventLists(page,size,searchText);
+			totalCount = eventService.getTotalCount();
+		}else {
+			events =	eventService.searchKeywordEventLists(page,size,startDate,endDate,searchText);
+			totalCount = eventService.getSearchTotalCount(startDate,endDate);	
+		}
 		
+		//리스트에 결과저장 (JSON형태)
 		Map<String, Object>response = new HashMap<String, Object>();
 		response.put("events", events);
 		response.put("totalCount", totalCount);
-		System.out.println(events);
+		
+		
 		return response;
 	}
+	
+	
  
 } // class

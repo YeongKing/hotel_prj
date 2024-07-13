965480dd191cd11db5db70d20c9d19ae7f155841
YeongKing <whdcks208@naver.com>
2024-07-13 21:26:18 +0900

    컨테이너 및 이미지명 수정

--- local-build-and-run.ps1
+++ local-build-and-run.ps1
@@ -2,13 +2,13 @@ Write-Host "1. Running GitHub Actions workflow..." -ForegroundColor Yellow
 act push -v
 
 Write-Host "2. Checking and building Docker image..." -ForegroundColor Yellow
-$imageExists = docker images -q my-java-app
+$imageExists = docker images -q elysian
 if (-not $imageExists) {
     Write-Host "Docker image does not exist. Building a new one." -ForegroundColor Yellow
-    docker build --no-cache -t my-java-app .
+    docker build --no-cache -t elysian .
 } else {
     Write-Host "Using existing Docker image." -ForegroundColor Green
-    docker build --no-cache -t my-java-app .
+    docker build --no-cache -t elysian .
 }
 
 if ($LASTEXITCODE -eq 0) {
@@ -21,19 +21,19 @@ if ($LASTEXITCODE -eq 0) {
         docker rm $containersUsingPort
     }
 
-    Write-Host "5. Stopping and removing existing my-java-app-container..." -ForegroundColor Yellow
-    docker stop my-java-app-container 2>$null
-    docker rm my-java-app-container 2>$null
+    Write-Host "5. Stopping and removing existing elysian-container..." -ForegroundColor Yellow
+    docker stop elysian-container 2>$null
+    docker rm elysian-container 2>$null
 
     Write-Host "6. Running new Docker container..." -ForegroundColor Yellow
-    docker run -d -p 80:8080 --name my-java-app-container my-java-app
+    docker run -d -p 80:8080 --name elysian-container elysian
 
     Start-Sleep -Seconds 10  # Wait 10 seconds for the container to fully start
 
     Write-Host "7. Application is running at http://stu14.sist.co.kr:80/hotel_prj/index.html" -ForegroundColor Green
     
     # Check container status
-    $containerStatus = docker inspect -f '{{.State.Status}}' my-java-app-container
+    $containerStatus = docker inspect -f '{{.State.Status}}' elysian-container
     if ($containerStatus -eq "running") {
         Write-Host "8. Container is running successfully." -ForegroundColor Green
     } else {
